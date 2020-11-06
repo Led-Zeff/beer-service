db4ba79fb1c0a48cc9cac0ad94b4fea7a7db17cb
Led-Zeff <ortega.victor.t.v@gmail.com>
2020-11-06 11:28:40 -0600

    Investory DTO & properties

--- src/main/java/courses/microservices/beerservice/service/inventory/model/BeerInventoryDto.java
+++ src/main/java/courses/microservices/beerservice/service/inventory/model/BeerInventoryDto.java
@@ -1,9 +1,19 @@
 package courses.microservices.beerservice.service.inventory.model;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class BeerInventoryDto {
+  private UUID id;
+  private OffsetDateTime createdDate;
+  private OffsetDateTime lastModifiedDate;
+  private UUID beerId;
+  private String upc;
   private Integer quantityOnHand;
 }
