387f078f5c749381564dbf1e8ad029b17edcd1bf
Led-Zeff <ortega.victor.t.v@gmail.com>
2020-11-06 17:13:34 -0600

    Missing decoratorr annotation

--- src/main/java/courses/microservices/beerservice/mapper/BeerMapper.java
+++ src/main/java/courses/microservices/beerservice/mapper/BeerMapper.java
@@ -1,11 +1,13 @@
 package courses.microservices.beerservice.mapper;
 
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
 import courses.microservices.beerservice.domain.Beer;
 import courses.microservices.beerservice.model.BeerDto;
 
 @Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
   BeerDto beerToDto(Beer beer);
   BeerDto beerToDtoWithInventory(Beer beer);
